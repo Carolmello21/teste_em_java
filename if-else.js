6147b894562226d1ec8338ded28885e26d4cd48c
Carol Melo <carolimavalente200859@gmail.com>
2022-08-25 17:52:03 -0300

    if-else

---
+++ if-else.js
@@ -0,0 +1,21 @@
+/*
+Calcular a média aritmética das notas do aluno e dizer se foi
+aprovado ou não com uma mensagem de apoio
+*/
+
+let n1 = 7.5;
+let n2 = 8.0;
+let media = (n1+n2)/2;
+console.log(`Sua nota final foi: ${media}`)
+
+if (media >= 7) {
+ if (media == 10) {
+    console.log ("Pense como se garantiu!!!");
+ } else if
+    (media > 8)
+    console.log ("Você passou, mas gostaria que repetisse o capítulo");
+ else {
+    console.log ("Infelizmente terá que refazer o teste");
+}
+
+}
