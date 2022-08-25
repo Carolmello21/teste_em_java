3ee7ae52d7f221272ad3d1e34832190ed127cb31
Carol Melo <carolimavalente200859@gmail.com>
2022-08-25 16:08:52 -0300

    conceitos

---
+++ conceitos.js
@@ -0,0 +1,34 @@
+// Conceitos em JavaScript lecionados no Treinamento C3C Software
+
+// Variáveis
+
+/*  var --> variável global: pode ser alterada em qualquer ponto do
+código
+ 
+    let --> variável definida por escopo; fora do bloco é GLOBAL;
+    dentro do bloco é LOCAL (pertence somente ao bloco)
+
+Constantes 
+
+    const --> constante; não aceita que seu valor seja alterado/modificado
+
+Concatenação
+
+--> Quando queremos usar informações em formato de texto e também 
+valor de código/variável
+
+String Template
+
+*/
+ let nome = "Carol";
+ console.log(`Meu nome é: ${nome}`);
+
+ // Operações 
+
+ /*
+ 1 --> console.log (( 5 * (4 + 32))) --> será executada a adição
+ primeiro
+
+ 2 --> console.log (( 5 * 4 + 32)) --> será executada a multiplicação 
+ primeiro 
+ */
